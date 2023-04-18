dfa23927b8813e3feb58508dd489d8be3bb94ef1
Balázs Réka <balazsreka@ktch.hu>
2023-04-18 18:55:44 +0200

    Szegély rendben

--- src/views/InputPanel.java
+++ src/views/InputPanel.java
@@ -4,6 +4,8 @@ import javax.swing.BoxLayout;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.border.EmptyBorder;
+
 import java.awt.Dimension; //ezt magától nem ajánlja fel. Ha valamit nem ajánl fel, jo eséllyel awt-s csomagban van.
 //kezdetben volt az awt, aztán a swing- minden operációs rendszer ugyanúgy néz ki, ha ebben programozunk, aztán az swt-n projekt, IBM-től, és a JavaFx- a Swing leváltására találták ki, kivették a jávából.
 
@@ -15,20 +17,28 @@ public class InputPanel extends JPanel {
             this.initComponent();
             this.setupComponent();
             this.addComponent();
+            this.setupPanel();
+            
 
         }
         public InputPanel(String text) {
             this.initComponent();
             this.setupComponent();
             this.label.setText(text);
+            this.setupPanel();
 
         }
         private void setupComponent(){
            
             this.label.setPreferredSize(new Dimension(100,27));
             this.field.setPreferredSize(new Dimension(100,27));
+        
 
         }
+       private void setupPanel(){
+        this.setBorder(new EmptyBorder(5,10,5,10));
+
+       }
 
         private void initComponent(){
             this.label = new JLabel();
@@ -52,6 +62,7 @@ public class InputPanel extends JPanel {
             this.setLayout(new BoxLayout(this,BoxLayout.LINE_AXIS));
             this.add(this.label);
             this.add(this.field);
+            
         }
     }
     
